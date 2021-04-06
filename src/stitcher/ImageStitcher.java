7498a0de49fb697e493a1fa63751031d8a8f3c9e
dan212 <danis21250@gmail.com>
2021-04-07 01:44:10 +0300

    Add files via upload

---
+++ src/stitcher/ImageStitcher.java
@@ -0,0 +1,77 @@
+package stitcher;
+
+import java.awt.Graphics;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.imageio.ImageIO;
+
+public class ImageStitcher {
+
+	public enum Direction{
+		DOWN,
+		LEFT
+	}
+	
+	private Vector<String> imagePaths;
+	private String endPath;
+	Direction dir;
+	BufferedImage combined;
+
+	public ImageStitcher(Vector<String> imagePaths, String end, Direction d) {
+		this.imagePaths = imagePaths;
+		endPath = end;
+		dir = d;
+	}
+	public Vector<String> getImagePaths() {
+		return imagePaths;
+	}
+
+	public void setImagePaths(Vector<String> imagePaths) {
+		this.imagePaths = imagePaths;
+	}
+	
+	public void combineAll() {
+		combined = new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB);
+		BufferedImage temp;
+		BufferedImage curImage = new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB);
+		for (String path : imagePaths) {
+			try {
+				 curImage = ImageIO.read(new File(path));
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			int w = Math.max(getCombined().getWidth(), curImage.getWidth());
+			int h = getCombined().getHeight() + curImage.getHeight();
+			temp = new BufferedImage(w,h,BufferedImage.TYPE_INT_ARGB);
+			Graphics g = temp.getGraphics();  
+			g.drawImage(getCombined(),0,0,null);
+			switch (dir) {
+				case DOWN :
+					g.drawImage(curImage,0,getCombined().getHeight(),null);
+					break;
+				case LEFT:
+					g.drawImage(curImage,getCombined().getWidth(),0,null);
+					break;
+				default:
+			}
+			combined = temp;
+			g.dispose();
+		}
+		try {
+			ImageIO.write(combined,"PNG", new File(endPath, "combined.png"));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		combined.flush();
+	}
+
+	public BufferedImage getCombined() {
+		return combined;
+	}
+	
+}
---
+++ src/stitcher/Stitcher.java
@@ -0,0 +1,99 @@
+package stitcher;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.image.*;
+import java.io.IOException;
+
+public class Stitcher {
+
+	JFrame frame;
+	JTextField inputRootField;
+	JTextField outputLocationField;
+	JButton stitchButton;
+	JPanel control;
+	String rootPath;
+	String outputPath;
+	FilePile fp = null;
+	ImageStitcher sch = null;
+	
+	private void UIInit() {
+		ActionHandler aHandler = new ActionHandler();
+		frame = new JFrame("Glue dem screencaps together dawg");
+		inputRootField = new JTextField(40);
+		outputLocationField = new JTextField(40);
+		inputRootField.setText("Path for pics go here");
+		outputLocationField.setText("Output Path (empty for same as input)");
+		stitchButton = new JButton("Stitch");
+		stitchButton.setActionCommand("stitch");
+		stitchButton.addActionListener(aHandler);
+		
+		control = new JPanel();
+		control.add(inputRootField);
+		control.add(outputLocationField);
+		control.add(stitchButton);
+		
+		frame.setLayout(new java.awt.BorderLayout());
+		frame.add(control,java.awt.BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setResizable(false);
+	}
+	
+	public void stitchAction() {
+		if(inputRootField.getText() != "") {
+			rootPath = inputRootField.getText();
+			if (outputLocationField.getText() != "") {
+				outputPath = rootPath;
+				outputLocationField.setText(outputPath);
+			}
+			if (fp == null) {
+				try {
+					fp  = new FilePile(rootPath);
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+			} else {
+				try {
+					fp.setRootPath(rootPath);
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+			
+			sch = new ImageStitcher(fp.getImagePaths(),outputPath,stitcher.ImageStitcher.Direction.DOWN);
+			sch.combineAll();
+		}
+		else {
+			outputLocationField.setText("Hey, set this first, dumbass!");
+		}
+	}
+	
+	
+	public class ActionHandler implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			if (e.getActionCommand() == "stitch") {
+				stitchAction();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		/*String path = "C:\\Users\\dan\\Desktop\\RGC_Resourses";
+		FilePile fp = null;
+		try {
+			fp  = new FilePile(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		fp.getImagePaths().forEach(System.out::println);*/
+		Stitcher st = new Stitcher();
+		st.UIInit();
+		
+	}
+
+}
